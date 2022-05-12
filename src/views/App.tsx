3a7c3341f44b19b5ab3bbf13580e8bf6bf700425
Dawid Słowik <dawid@MacBook-Air-Dawid.local>
2022-05-12 20:48:41 +0200

    Create Users List

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, {Fragment} from 'react';
+import styled from 'styled-components';
 
-function App() {
+// Global Styles
+import {GlobalStyle} from '../styles/GlobalStyles';
+import {ThemeProvider} from 'styled-components';
+import {theme} from 'styles/theme';
+
+// Components
+import UsersList from 'components/organisms/users-list/UsersList.component';
+
+const Wrapper = styled.div`
+  background: {
+    color: #f7f8fa;
+  };
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Root = () => {
   return (
-    <div className="App">
-      Studdy buddy
-    </div>
+     <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Wrapper>
+           <UsersList />
+        </Wrapper>
+     </ThemeProvider>
+
   );
 }
 
-export default App;
+export default Root;
