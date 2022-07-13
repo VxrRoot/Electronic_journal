552895badd255df2b74010b779b0e5ad2798afa1
Dawid Słowik <dawid.slowik@visionect.io>
2022-07-14 00:48:41 +0200

    Implement useReducer and custom hook to add user

--- src/views/add-user/AddUser.model.ts
+++ src/views/add-user/AddUser.model.ts
@@ -4,4 +4,20 @@ interface IAddUserModel {
    
 }
 
+interface IReducerState {
+   name: string;
+   average: string;
+   attendance: string;
+}
+
+interface IReducerAction {
+   type: 'INPUT CHANGE' | 'CLEAR VALUES';
+   field: string;
+   value: string;
+}
+
+export type TypeReducerAction = IReducerAction;
+
+export type TypeReducerState = IReducerState;
+
 export type AddUserModel = IAddUserModel
